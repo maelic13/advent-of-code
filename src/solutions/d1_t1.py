f821f3752cb13eba9aabc77dbd9e9af7fc4cfba7
Miloslav Macůrek <macurek.m@gmail.com>
2020-12-02 16:48:11 +0100

    Fix typing

--- src/solutions/d1_t1.py
+++ src/solutions/d1_t1.py
@@ -1,20 +1,20 @@
 from itertools import combinations
-from numpy import prod
-from typing import Optional
+from numpy import ndarray, prod
+from typing import Callable, Iterable, Optional
 
 from src.infra.api import DataReader
 
 
-def expense_record(file_name: str, number_of_items: int) -> Optional:
+def expense_record(file_name: str, number_of_items: int) -> Optional[ndarray]:
     data = DataReader.read_txt(file_name, int)
     for item in combinations(data, number_of_items):
-        if check_addition(item, 2020):
+        if check_item(item, sum, 2020):
             return prod(item)
     return None
 
 
-def check_addition(iterable, target):
-    return sum(iterable) == target
+def check_item(iterable: Iterable, function: Callable, target: float) -> bool:
+    return function(iterable) == target
 
 
 if __name__ == "__main__":
