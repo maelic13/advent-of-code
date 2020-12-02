83178dad6cc292e2591c15c82c198aa7f7fd63c1
Miloslav Macůrek <macurek.m@gmail.com>
2020-12-02 16:44:50 +0100

    Day1Task2

--- src/solutions/d1_t1.py
+++ src/solutions/d1_t1.py
@@ -1,8 +1,22 @@
+from itertools import combinations
+from numpy import prod
+from typing import Optional
+
 from src.infra.api import DataReader
 
-data = DataReader.read_txt("d1_t1.txt", int)
-while data:
-    number = data.pop()
-    if 2020 - number in data:
-        print("Result is: {}".format((2020 - number) * number))
-        break
+
+def expense_record(file_name: str, number_of_items: int) -> Optional:
+    data = DataReader.read_txt(file_name, int)
+    for item in combinations(data, number_of_items):
+        if check_addition(item, 2020):
+            return prod(item)
+    return None
+
+
+def check_addition(iterable, target):
+    return sum(iterable) == target
+
+
+if __name__ == "__main__":
+    solution = expense_record("d1_t1.txt", 2)
+    print("Solution is: {}".format(solution))
---
+++ src/solutions/d1_t2.py
@@ -0,0 +1,6 @@
+from src.solutions.d1_t1 import expense_record
+
+
+if __name__ == "__main__":
+    solution = expense_record("d1_t1.txt", 3)
+    print("Solution is: {}".format(solution))
