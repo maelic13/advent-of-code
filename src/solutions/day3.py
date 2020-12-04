b4710de8e71c97cc3cd5d1210c32fdd286c1c5f5
Miloslav Macůrek <macurek.m@gmail.com>
2020-12-04 23:00:30 +0100

    Day3 - class to function

--- src/solutions/day3.py
+++ src/solutions/day3.py
@@ -5,26 +5,22 @@ from typing import List
 from src.infra.api import DataReader
 
 
-class TobogganRouteTracker:
-    def __init__(self, tree_map: List) -> None:
-        self._map = tree_map
-
-    def count_trees_hit(self, current_pos: np.ndarray, slope: np.ndarray) -> int:
-        slope = slope / gcd(slope[0], slope[1])
-        trees_hit = 0
-        while current_pos[1] < len(self._map):
-            x = int(current_pos[1])
-            y = int(current_pos[0]) % len(self._map[x])
-            if self._map[x][y] == "#":
-                trees_hit += 1
-            current_pos = np.add(current_pos, slope)
-        return trees_hit
+def count_trees_hit(slope_map: List, current_pos: np.ndarray, slope: np.ndarray) -> int:
+    slope = slope / gcd(slope[0], slope[1])
+    trees_hit = 0
+    while current_pos[1] < len(slope_map):
+        x = int(current_pos[1])
+        y = int(current_pos[0]) % len(slope_map[x])
+        if slope_map[x][y] == "#":
+            trees_hit += 1
+        current_pos = np.add(current_pos, slope)
+    return trees_hit
 
 
 if __name__ == "__main__":
-    input_data = DataReader.read_txt("day3.txt", str)
+    input_data = DataReader.read_txt("day3_brother.txt", str)
 
-    result = TobogganRouteTracker(input_data).count_trees_hit(np.array((0, 0)), np.array((3, 1)))
+    result = count_trees_hit(input_data, np.array((0, 0)), np.array((3, 1)))
     print("With slope {} you hit {} trees.".format((3, 1), result))
 
     slopes = [
@@ -34,5 +30,5 @@ if __name__ == "__main__":
         np.array((1, 2))
     ]
     for single_slope in slopes:
-        result *= TobogganRouteTracker(input_data).count_trees_hit(np.array((0, 0)), single_slope)
+        result *= count_trees_hit(input_data, np.array((0, 0)), single_slope)
     print("With multiple additional slopes, result is {} trees.".format(result))
