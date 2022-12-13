07e8f2477e61f95e30a4cc0e7e729714c960ee0c
Miloslav Macůrek <miloslav.macurek@thermofisher.com>
2022-12-13 12:22:04 +0100

    Day 13 - rename

--- src/year2022/day13.py
+++ src/year2022/day13.py
@@ -2,17 +2,20 @@ from functools import cmp_to_key
 import json
 
 
-def in_order(item1, item2) -> int:
+def compare(item1, item2) -> int:
+    """
+    Compare items, negative means item1 is lower, 0 equal and positive larger than item2.
+    """
     if isinstance(item1, int) and isinstance(item2, int):
         return 0 if item1 == item2 else item1 - item2
 
     if isinstance(item1, int):
-        return in_order([item1], item2)
+        return compare([item1], item2)
     if isinstance(item2, int):
-        return in_order(item1, [item2])
+        return compare(item1, [item2])
 
     for i in range(min([len(item1), len(item2)])):
-        order = in_order(item1[i], item2[i])
+        order = compare(item1[i], item2[i])
         if order == 0:
             continue
         return order
@@ -30,12 +33,12 @@ def advent13() -> None:
         i += 3
 
     # part 1
-    in_order_indices: list[int] = []
+    compare_indices: list[int] = []
     for index, packet in enumerate(packets, start=1):
         item1, item2 = packet
-        if in_order(item1, item2) < 0:
-            in_order_indices.append(index)
-    print(sum(in_order_indices))
+        if compare(item1, item2) < 0:
+            compare_indices.append(index)
+    print(sum(compare_indices))
 
     # part 2
     single_packets = [[[2]], [[6]]]
@@ -43,7 +46,7 @@ def advent13() -> None:
         if line == "\n":
             continue
         single_packets.append(json.loads(line.strip()))
-    single_packets.sort(key=cmp_to_key(in_order))
+    single_packets.sort(key=cmp_to_key(compare))
     print((single_packets.index([[2]]) + 1) * (single_packets.index([[6]]) + 1))
 
 
