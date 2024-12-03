997540b25bf5023791227eba6679e3c0d857984b
Miloslav Macůrek <macurek.m@gmail.com>
2024-12-03 16:57:22 +0100

    2024: Day 3 Part 2 in Rust

--- rust/year2024/day3.rs
+++ rust/year2024/day3.rs
@@ -4,12 +4,24 @@ use std::io::{BufRead, BufReader};
 use regex::Regex;
 use simple_stopwatch::Stopwatch;
 
-fn section_analysis(section: &String) -> isize {
-    let mut result: isize = 0;
+fn advanced_section_analysis(section: &String) -> usize {
+    let mut result: usize = 0;
+
+    for part in section.split("do()") {
+        result += section_analysis(
+            &part.split("don't()").next().unwrap().parse::<String>().unwrap()
+        );
+    }
+
+    return result;
+}
+
+fn section_analysis(section: &String) -> usize {
+    let mut result: usize = 0;
 
     let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
     for mul_args in re.captures_iter(section) {
-        result += (&mul_args[1]).parse::<isize>().unwrap() * (&mul_args[2]).parse::<isize>().unwrap();
+        result += &mul_args[1].parse::<usize>().unwrap() * &mul_args[2].parse::<usize>().unwrap();
     }
 
     return result;
@@ -22,20 +34,18 @@ fn main() {
     let file = File::open("../inputs/2024/day3.txt").unwrap();
     let reader = BufReader::new(file);
 
-    let memory_sections: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
+    let memory_sections: String =
+        reader.lines()
+            .map(|l| l.unwrap())
+            .collect::<Vec<_>>().join("");
     let file_read_time = watch.us();
 
     // part 1
-    let mut result: isize = 0;
-    for section in memory_sections {
-        result += section_analysis(&section);
-    }
-    println!("{}", result);
+    println!("{}", section_analysis(&memory_sections));
     let part1_time = watch.us() - file_read_time;
 
     // part 2
-    let mut result: usize = 0;
-    println!("{}", result);
+    println!("{}", advanced_section_analysis(&memory_sections));
     let part2_time = watch.us() - part1_time - file_read_time;
 
     // report times
