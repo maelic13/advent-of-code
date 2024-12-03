26a8ef8b9e8716829d19d194627715cbec53a764
Miloslav Macůrek <miloslav.macurek@thermofisher.com>
2024-12-03 11:43:03 +0100

    Split python 2024 Day 2 into two solutions

--- python/year2024/day2.py
+++ python/year2024/day2.py
@@ -1,14 +1,13 @@
 from time import time_ns
 
 
-def is_report_safe(rep: list[int]) -> bool:
+def is_report_safe(rep: list[int], dampened: bool = True) -> bool:
     if len(rep) < 2:
         return True
 
     if rep[1] < rep[0]:
-        rep.reverse()
+        rep = list(reversed(rep))
 
-    dampened = False
     for i in range(len(rep) - 1):
         if rep[i] > rep[i + 1] or not 1 <= (rep[i + 1] - rep[i]) <= 3:
             if dampened:
@@ -28,6 +27,9 @@ def day2() -> None:
 
     # part 1
     print(sum(is_report_safe(report) for report in reports))
+
+    # part 2
+    print(sum(is_report_safe(report, dampened=False) for report in reports))
 
 if __name__ == "__main__":
     start = time_ns()
