3708415230a74be68f1ce6d158a071145849c288
Miloslav Macůrek <macurek.m@gmail.com>
2024-12-16 22:19:12 +0100

    2024: Day 9 Part 1 in Rust, Part 2 WIP

--- rust/year2024/day9.rs
+++ rust/year2024/day9.rs
@@ -3,29 +3,132 @@ use std::io::{BufRead, BufReader};
 
 use simple_stopwatch::Stopwatch;
 
-fn calculate_checksum(disk: &Vec<char>) -> usize {
+fn calculate_checksum(disk: &Vec<String>) -> usize {
     let mut checksum = 0;
     for (i, char) in disk.iter().enumerate() {
-        if *char == '.' { continue; }
-        checksum += i * char.to_digit(10).unwrap() as usize;
+        if char == "." { continue; }
+        checksum += i * char.parse::<usize>().unwrap();
     }
     checksum
 }
 
-fn move_files_to_front(disk: &mut Vec<char>) {}
+fn move_bits_to_front(disk: &mut Vec<String>) {
+    let disk_len: usize = disk.len();
+    let disk_last_index: usize = disk_len - 1;
 
-fn translate_disk_map(disk_map: &Vec<char>) -> Vec<char> {
-    let mut disk = vec![];
+    for i in 0..disk_len {
+        if disk[i] != "." { continue; }
+        for j in 0..disk_len {
+            if disk_last_index - j <= i { break; }
+            if disk[disk_last_index - j] != "." {
+                disk.swap(i, disk_last_index - j);
+                break;
+            }
+        }
+    }
+}
+
+fn move_files_to_front(disk: &mut Vec<String>) {
+    let disk_len: usize = disk.len();
+    let disk_last_index: usize = disk_len - 1;
+    let mut last_checked_index: usize = disk_last_index;
+
+    for i in 0..disk_last_index {
+        if disk[i] != "." { continue; }
+
+        let mut empty_space_size = 0;
+        for j in 1..disk_last_index {
+            if disk[i + j] != "." {
+                empty_space_size += j;
+                break;
+            }
+        }
+
+        let mut current_file: String = String::from("0");
+        for j in 0..last_checked_index {
+            if last_checked_index - j <= i { break; }
+
+            let char: String = disk[last_checked_index - j].to_string();
+            if char == current_file || char == "." { continue; };
+            current_file = char;
+
+            let mut file_size: usize = 0;
+            for k in 1..disk_len {
+                if disk[last_checked_index - j - k] != current_file {
+                    file_size += k;
+                    break;
+                }
+            }
+            if file_size > empty_space_size { continue; }
+            for k in 0..file_size {
+                println!("Moving {} to {}", i + k, disk_len - j - file_size + k);
+                disk.swap(i + k, disk_len - j - file_size + k);
+            }
+            last_checked_index = last_checked_index - j - file_size;
+            break;
+        }
+        print_disk(&disk);
+    }
+}
+
+fn move_files_to_front2(disk: &mut Vec<String>) {
+    let disk_len: usize = disk.len();
+    let disk_last_index: usize = disk_len - 1;
+    let mut current_file: String;
+
+    for i in 0..disk_len {
+        let char = disk[disk_last_index - i].to_string();
+        if char == "." { continue; }
+
+        current_file = char;
+        let mut file_size: usize = 0;
+        for j in 0..disk_last_index {
+            if disk[disk_last_index - i - j] != current_file {
+                file_size += j;
+                break;
+            }
+        }
+
+        for j in 0..disk_len - file_size {
+            if j > i { break; }
+            if disk[j] != "." { continue; }
+            let mut empty_space_size: usize = 0;
+            for k in 0..file_size {
+                if disk[j + k] != "." { break; }
+                empty_space_size = k + 1;
+            }
+            println!("Empty size: {}, file size: {}", empty_space_size, file_size);
+            if empty_space_size < file_size{ continue; }
+
+            for k in 0..file_size {
+                disk.swap(j + k, disk_len - i - file_size + k);
+            }
+            break;
+        }
+
+        print_disk(&disk);
+    }
+}
+
+fn translate_disk_map(disk_map: &Vec<char>) -> Vec<String> {
+    let mut disk: Vec<String> = vec![];
     for (i, char) in disk_map.iter().enumerate() {
-        let mut character = '.';
+        let mut character = ".".to_string();
         if i % 2 == 0 {
-            character = char::from_digit((i / 2) as u32, 10).unwrap();
+            character = (i / 2).to_string();
         }
         for _ in 0..char.to_digit(10).unwrap() {
-            disk.push(character);
+            disk.push(character.clone());
         }
     }
     disk
+}
+
+fn print_disk(disk: &Vec<String>) {
+    for char in disk {
+        print!("{}", char);
+    }
+    println!();
 }
 
 fn main() {
@@ -45,15 +148,19 @@ fn main() {
     }
     let file_read_time = watch.us();
 
+    let mut disk: Vec<String> = translate_disk_map(&disk_map);
+
     // part 1
-    let mut disk = translate_disk_map(&disk_map);
-    move_files_to_front(&mut disk);
-    println!("{}", calculate_checksum(&disk));
+    let mut modified_disk = disk.to_vec();
+    move_bits_to_front(&mut modified_disk);
+    println!("{}", calculate_checksum(&modified_disk));
     let part1_time = watch.us() - file_read_time;
 
     // part 2
-    let result: isize = 0;
-    println!("{}", result);
+    print_disk(&disk);
+    move_files_to_front2(&mut disk);
+    print_disk(&disk);
+    println!("{}", calculate_checksum(&disk));
     let part2_time = watch.us() - part1_time - file_read_time;
 
     // report times
