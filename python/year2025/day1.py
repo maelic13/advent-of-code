74ef364450292cd502eb01dce8721943e8595a31
Miloslav Macůrek <macurek.m@gmail.com>
2025-12-02 10:18:24 +0100

    2025: Day 1 + 2 in Python

---
+++ python/year2025/day1.py
@@ -0,0 +1,41 @@
+from time import time_ns
+
+
+def calculate_move(command: str) -> int:
+    if command[0] == "R":
+        return int(command[1:])
+    return -int(command[1:])
+
+
+def day1() -> None:
+    with open("inputs/2025/day1.txt", "r") as file:
+        lines = file.readlines()
+
+    # part 1
+    current_position = 50
+    password_1 = 0
+    for line in lines:
+        current_position = (current_position + calculate_move(line.strip())) % 100
+        if current_position == 0:
+            password_1 += 1
+    print(password_1)
+
+    # part 2
+    current_position = 50
+    password_2 = 0
+    for line in lines:
+        for _ in range(int(line.strip()[1:])):
+            if line[0] == "R":
+                current_position += 1
+            else:
+                current_position -= 1
+            current_position %= 100
+            if current_position == 0:
+                password_2 += 1
+    print(password_2)
+
+
+if __name__ == "__main__":
+    start = time_ns()
+    day1()
+    print(f"Execution time: {round((time_ns() - start) // 1000000)} milliseconds.")
---
+++ python/year2025/day2.py
@@ -0,0 +1,49 @@
+from time import time
+from regex import findall
+
+
+def is_id_invalid(i: int) -> bool:
+    id_string = str(i)
+    if len(id_string) % 2 != 0:
+        return False
+
+    return id_string[:len(id_string) // 2] == id_string[len(id_string) // 2:]
+
+
+def is_id_invalid_2(number: int) -> bool:
+    id_string = str(number)
+    match = findall(r"^(.+)\1+$", id_string)
+    return bool(match)
+
+
+def day2() -> None:
+    with open("inputs/2025/day2.txt", "r") as file:
+        lines = file.readlines()
+
+    # part 1
+    invalid_ids: set[int] = set()
+    for string in lines[0].strip().split(","):
+        begin, end = string.split("-")
+        for i in range(int(begin), int(end) + 1):
+            if i in invalid_ids:
+                continue
+            if is_id_invalid(i):
+                invalid_ids.add(i)
+    print(sum(invalid_ids))
+
+    # part 2
+    invalid_ids: set[int] = set()
+    for string in lines[0].strip().split(","):
+        begin, end = string.split("-")
+        for i in range(int(begin), int(end) + 1):
+            if i in invalid_ids:
+                continue
+            if is_id_invalid_2(i):
+                invalid_ids.add(i)
+    print(sum(invalid_ids))
+
+
+if __name__ == "__main__":
+    start = time()
+    day2()
+    print(f"Execution time: {round((time() - start), 1)} seconds.")
